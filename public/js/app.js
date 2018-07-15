657583e8f5154b40fb2497b3d0dbf42385df09c0
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-14 18:16:06 -1000

    lists posts

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,54 @@
 console.log('test •');
 
-const getOutside = new XMLHttpRequest();
-getOutside.addEventListener('load', outside);
-getOutside.open('GET', 'https://www.reddit.com/r/WTF/.json');
-getOutside.send();
+const getWTF = new XMLHttpRequest();
+getWTF.addEventListener('load', outside);
+getWTF.open('GET', 'https://www.reddit.com/r/WTF/.json');
+getWTF.send();
 
 let getContentDiv = document.getElementById('body-div');
 
 
 function outside() {
-  const parsedOutside = JSON.parse(this.responseText);
-  console.log(parsedOutside);
+  const parsedWTF = JSON.parse(this.responseText);
+  console.log(parsedWTF);
 
-  const outsidePosts = parsedOutside.data.children;
+  const wtfPosts = parsedWTF.data.children;
 
-  for (let i = 0; i < outsidePosts.length; i++) {
+
+  for (let i = 0; i < wtfPosts.length; i++) {
+
+    let postDiv = document.createElement('div');
+    postDiv.className = 'posts';
+    getContentDiv.appendChild(postDiv);
+
+    //get picture
+
+    let pictureDiv = document.createElement('div');
+    pictureDiv.className = 'picture';
+    if (wtfPosts[i].data.thumbnail === 'nsfw'){
+      pictureDiv.innerHTML = 'NSFW';
+    }
+    pictureDiv.style.backgroundImage = 'url("' + wtfPosts[i].data.thumbnail + '")'
+    postDiv.appendChild(pictureDiv);
+
     //get title
-    let contentDiv = document.createElement('div');contentDiv.className = 'content';
-    contentDiv.innerHTML = outsidePosts[i].data.title;
-    getContentDiv.appendChild(contentDiv);
+    let titleDiv = document.createElement('div'); titleDiv.className = 'title';
+    titleDiv.innerHTML = wtfPosts[i].data.title;
+    postDiv.appendChild(titleDiv);
 
     //get post stats
     let redditor = document.createElement('p');
     redditor.className = 'stats';
-    redditor.innerHTML = 'by ' + outsidePosts[i].data.author + ' • ' + 'Score ' + outsidePosts[i].data.score + ' • ' + outsidePosts[i].data.num_comments + ' comments';
-    contentDiv.appendChild(redditor);
+    redditor.innerHTML = 'by ' + wtfPosts[i].data.author + ' • ' + 'Score ' + wtfPosts[i].data.score + ' • ' + wtfPosts[i].data.num_comments + ' comments';
+    postDiv.appendChild(redditor);
 
-    //get 4 lines of post text
+
+    //get post text
     let textBody = document.createElement('div');
     textBody.className = 'text-body';
-    textBody.innerHTML = outsidePosts[i].data.selftext;
-    console.log(outsidePosts[i].data.selftext);
-    contentDiv.appendChild(textBody);
+    textBody.innerHTML = wtfPosts[i].data.selftext;
+    postDiv.appendChild(textBody);
+
   }
 
 }
