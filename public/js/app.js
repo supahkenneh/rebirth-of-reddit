4f73273866c4a1192b90bab717d3d2351f95758a
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-16 11:16:44 -1000

    fixed formatting issues with pics and text

--- public/js/app.js
+++ public/js/app.js
@@ -33,7 +33,7 @@ function showCity() {
 
       let pictureDiv = document.createElement('div');
       pictureDiv.className = 'picture';
-      if (cityPosts[i].data.url.split('').reverse().join('').charAt(0) !== 'g'){
+      if (cityPosts[i].data.url.split('').reverse().join('').charAt(0) !== 'g') {
         pictureDiv.style.backgroundImage = 'url("https://b.thumbs.redditmedia.com/1A0mtan1uVG3Om-BfkpU_Fie3FuZ20ZmUawhNOo4x0c.jpg")'
       } else {
         pictureDiv.style.backgroundImage = 'url("' + cityPosts[i].data.url + '")'
@@ -44,9 +44,6 @@ function showCity() {
       let titleDiv = document.createElement('div'); titleDiv.className = 'title';
       titleDiv.innerHTML = cityPosts[i].data.title;
       postDiv.appendChild(titleDiv);
-      if(titleDiv.innerHTML.length > 100){
-        titleDiv.style.fontSize = '14px';
-      }
 
       //get post stats
       let redditor = document.createElement('p');
@@ -60,89 +57,30 @@ function showCity() {
       textBody.className = 'text-body';
       textBody.innerHTML = cityPosts[i].data.selftext;
       postDiv.appendChild(textBody);
-
     }
   }
-
 }
 
 const dadButton = document.getElementById('subreddit-two');
 dadButton.addEventListener('click', showDadJokes);
 
-function showDadJokes(){
+function showDadJokes() {
 
   getContentDiv.innerHTML = '';
-  
+
   const getDadJokes = new XMLHttpRequest();
   getDadJokes.addEventListener('load', dadJokesSubreddit);
   getDadJokes.open('GET', 'https://www.reddit.com/r/dadjokes/.json');
   getDadJokes.send();
 
-  function dadJokesSubreddit(){
+  function dadJokesSubreddit() {
     const parsedDads = JSON.parse(this.responseText);
 
     const dadPosts = parsedDads.data.children;
 
     console.log(parsedDads);
 
-    for (let i = 0 ; i < dadPosts.length; i++){
-
-      let postDiv = document.createElement('div');
-      postDiv.className = 'posts';
-      getContentDiv.appendChild(postDiv);
-
-       //get picture
-       let pictureDiv = document.createElement('div');
-       pictureDiv.className = 'picture';
-       if (dadPosts[i].data.url.split('').reverse().join('').charAt(0) !== 'g'){
-         pictureDiv.style.backgroundImage = 'url("https://b.thumbs.redditmedia.com/1A0mtan1uVG3Om-BfkpU_Fie3FuZ20ZmUawhNOo4x0c.jpg")'
-       } else {
-         pictureDiv.style.backgroundImage = 'url("' + dadPosts[i].data.url + '")'
-       }
-       postDiv.appendChild(pictureDiv);
- 
-       //get title
-       let titleDiv = document.createElement('div'); titleDiv.className = 'title';
-       titleDiv.innerHTML = dadPosts[i].data.title;
-       postDiv.appendChild(titleDiv);
- 
-       //get post stats
-       let redditor = document.createElement('p');
-       redditor.className = 'stats';
-       redditor.innerHTML = 'by ' + dadPosts[i].data.author + ' • ' + 'Upvotes ' + dadPosts[i].data.score + ' • ' + dadPosts[i].data.num_comments + ' comments';
-       postDiv.appendChild(redditor);
- 
- 
-       //get post text
-       let textBody = document.createElement('div');
-       textBody.className = 'text-body';
-       textBody.innerHTML = dadPosts[i].data.selftext;
-       postDiv.appendChild(textBody);
-
-    }
-  }
-}
-
-const showerButton = document.getElementById('subreddit-three');
-showerButton.addEventListener('click', showThoughts);
-
-function showThoughts () {
-    
-  getContentDiv.innerHTML = '';
-  
-  const getThoughts = new XMLHttpRequest();
-  getThoughts.addEventListener('load', showerThoughtsSubreddit);
-  getThoughts.open('GET', 'https://www.reddit.com/r/meirl/.json');
-  getThoughts.send();
-
-  function showerThoughtsSubreddit(){
-    const parsedThoughts = JSON.parse(this.responseText);
-
-    const thoughtPosts = parsedThoughts.data.children;
-
-    console.log(parsedThoughts);
-
-    for (let i = 0; i < thoughtPosts.length; i++){
+    for (let i = 0; i < dadPosts.length; i++) {
 
       let postDiv = document.createElement('div');
       postDiv.className = 'posts';
@@ -151,38 +89,88 @@ function showThoughts () {
       //get picture
       let pictureDiv = document.createElement('div');
       pictureDiv.className = 'picture';
-      if (thoughtPosts[i].data.url.split('').reverse().join('').charAt(0) !== 'g'){
-        pictureDiv.style.backgroundImage = 'url("https://b.thumbs.redditmedia.com/1A0mtan1uVG3Om-BfkpU_Fie3FuZ20ZmUawhNOo4x0c.jpg")'
+      if (dadPosts[i].data.url.split('').reverse().join('').charAt(0) !== 'g') {
+        pictureDiv.style.backgroundImage = 'url("http://theonyxpath.com/wp-content/uploads/2014/12/snoo.png")'
       } else {
-        pictureDiv.style.backgroundImage = 'url("' + thoughtPosts[i].data.url + '")'
+        pictureDiv.style.backgroundImage = 'url("' + dadPosts[i].data.url + '")'
       }
       postDiv.appendChild(pictureDiv);
 
       //get title
       let titleDiv = document.createElement('div'); titleDiv.className = 'title';
-      titleDiv.innerHTML = thoughtPosts[i].data.title;
+      titleDiv.innerHTML = dadPosts[i].data.title;
       postDiv.appendChild(titleDiv);
-      if(titleDiv.innerHTML.length > 100){
-        titleDiv.style.fontSize = '14px';
-      }
 
       //get post stats
       let redditor = document.createElement('p');
       redditor.className = 'stats';
-      redditor.innerHTML = 'by ' + thoughtPosts[i].data.author + ' • ' + 'Upvotes ' + thoughtPosts[i].data.score + ' • ' + thoughtPosts[i].data.num_comments + ' comments';
+      redditor.innerHTML = 'by ' + dadPosts[i].data.author + ' • ' + 'Upvotes ' + dadPosts[i].data.score + ' • ' + dadPosts[i].data.num_comments + ' comments';
+      postDiv.appendChild(redditor);
+
+      //get post text
+      let textBody = document.createElement('div');
+      textBody.className = 'text-body';
+      textBody.innerHTML = dadPosts[i].data.selftext;
+      postDiv.appendChild(textBody);
+    }
+  }
+}
+
+const meirlButton = document.getElementById('subreddit-three');
+meirlButton.addEventListener('click', showMeirl);
+
+function showMeirl() {
+
+  getContentDiv.innerHTML = '';
+
+  const getMeirl = new XMLHttpRequest();
+  getMeirl.addEventListener('load', meirlSubreddit);
+  getMeirl.open('GET', 'https://www.reddit.com/r/meirl/.json');
+  getMeirl.send();
+
+  function meirlSubreddit() {
+    const parsedMeirl = JSON.parse(this.responseText);
+
+    const meirlPosts = parsedMeirl.data.children;
+
+    console.log(parsedMeirl);
+
+    for (let i = 0; i < meirlPosts.length; i++) {
+
+      let postDiv = document.createElement('div');
+      postDiv.className = 'posts';
+      getContentDiv.appendChild(postDiv);
+
+      //get picture
+      let pictureDiv = document.createElement('div');
+      pictureDiv.className = 'picture';
+      if (meirlPosts[i].data.url.split('').reverse().join('').charAt(0) !== 'g') {
+        pictureDiv.style.backgroundImage = 'url("https://b.thumbs.redditmedia.com/1A0mtan1uVG3Om-BfkpU_Fie3FuZ20ZmUawhNOo4x0c.jpg")'
+      } else {
+        pictureDiv.style.backgroundImage = 'url("' + meirlPosts[i].data.url + '")'
+      }
+      postDiv.appendChild(pictureDiv);
+
+      //get title
+      let titleDiv = document.createElement('div'); titleDiv.className = 'title';
+      titleDiv.innerHTML = meirlPosts[i].data.title;
+      postDiv.appendChild(titleDiv);
+
+      //get post stats
+      let redditor = document.createElement('p');
+      redditor.className = 'stats';
+      redditor.innerHTML = 'by ' + meirlPosts[i].data.author + ' • ' + 'Upvotes ' + meirlPosts[i].data.score + ' • ' + meirlPosts[i].data.num_comments + ' comments';
       postDiv.appendChild(redditor);
 
 
       //get post text
       let textBody = document.createElement('div');
       textBody.className = 'text-body';
-      textBody.innerHTML = thoughtPosts[i].data.selftext;
-      if(textBody.innerHTML.length > 250){
+      textBody.innerHTML = meirlPosts[i].data.selftext;
+      if (textBody.innerHTML.length > 250) {
         textBody.innerHTML = textBody.innerHTML.substr(0, 200) + '...';
       }
       postDiv.appendChild(textBody);
-
-
     }
   }
 }
